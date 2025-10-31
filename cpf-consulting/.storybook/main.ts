49bdcc3cf354b6090dd5b19bfcb441fb2d981c3a
mahalonowino <mahowino@gmail.com>
2025-10-31 16:25:08 +0300

    Added story book with default component, to help in visualizing components and unit testing

---
+++ cpf-consulting/.storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+
+const config: StorybookConfig = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ]
+};
+export default config;
--- cpf-consulting/app/layout.tsx
+++ cpf-consulting/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Analytics from "@/Components/Analytics/Analytics";
+import Analytics from "@/components/Analytics/Analytics";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
